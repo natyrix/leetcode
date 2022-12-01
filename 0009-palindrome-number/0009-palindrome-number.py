ae2453a97f9c54fecc02a48a89315e0728655acb
Natnael Melese <36536193+natyrix@users.noreply.github.com>
2022-12-01 12:52:52 -0500

    Time: 108 ms (55.77%), Space: 13.3 MB (85.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            return x == int(str(x)[::-1])
+        
